72c6e32364a0659ab93ff7e8e541f2ff72660e40
handesaurabh <handesaurabh2004@gmail.com>
2025-10-19 11:01:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,60 +1,19 @@
 // Your code here.
-const container=document.querySelector(".items");
-const cubes =document.querySelectorAll(".item");
+const slider = document.querySelector('.items');
+let isDown = false, startX, scrollLeft;
 
-let selectedCube=null;
-let offsetX=0;
-let offsetY=0;
-
-cubes.forEach(cube=>{
-	cube.style.position="absolute";
-	const rect=cube.getBoundingClientRect();
-	const containerRect = container.getBoundingClientRect();
-	cube.style.left=`${rect.left-containerRect.left}px`;
-	cube.style.top=`${rect.top-containerRect.top}px`;
-
-	cube.addEventListener("mousedown",(e)=>{
-	selectedCube = cube;
-    const cubeRect = cube.getBoundingClientRect();
-    offsetX = e.clientX - cubeRect.left;
-    offsetY = e.clientY - cubeRect.top;
-    cube.style.zIndex = 1000; 
-    e.preventDefault();
-	});
+slider.addEventListener('mousedown', e => {
+  isDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
 });
 
-document.addEventListener('mousemove', (e) => {
-  if (!selectedCube) {
-	  return;
-  }
-	  
-  const containerRect = container.getBoundingClientRect();
-  const cubeRect = selectedCube.getBoundingClientRect();
-  let newLeft = e.clientX - containerRect.left - offsetX;
-  let newTop = e.clientY - containerRect.top - offsetY;
-  const maxLeft = containerRect.width - cubeRect.width;
-  const maxTop = containerRect.height - cubeRect.height;
+slider.addEventListener('mouseleave', () => isDown = false);
+slider.addEventListener('mouseup', () => isDown = false);
 
-  if (newLeft < 0) {
-	  newLeft = 0;
-  }
-  if (newTop < 0) {
-	  newTop = 0;
-  }
-  if (newLeft > maxLeft) {
-	  newLeft = maxLeft;
-  }
-  if (newTop > maxTop) {
-	  newTop = maxTop;
-  }
-
-  selectedCube.style.left = `${newLeft}px`;
-  selectedCube.style.top = `${newTop}px`;
-});
-
-document.addEventListener('mouseup', () => {
-  if (selectedCube) {
-    selectedCube.style.zIndex = '';
-    selectedCube = null;
-  }
+slider.addEventListener('mousemove', e => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  slider.scrollLeft = scrollLeft - (x - startX);
 });
